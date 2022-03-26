7dd3e1cbb1ef60249408982a1cb1b95c47dce123
Nelson Mendes Pereira <nmp2605@hotmail.com>
2022-03-26 13:58:38 -0300

    Add base files

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() < 1 {
+        panic!("The location to the chip8 ROM is required.");
+    }
+
+    let rom_path: &String = &args[0];
+
+    let contents: Vec<u8> = fs::read(rom_path)
+        .expect("The file path is invalid.");
+}
