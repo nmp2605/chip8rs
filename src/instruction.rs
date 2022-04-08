7eff86245fd64051d382c85e66a6dd84f054cac1
Nelson Mendes Pereira <nmp2605@hotmail.com>
2022-04-08 17:05:19 -0300

    Add instructions up to the draw instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,4 +1,5 @@
 use crate::cpu::Cpu;
+use rand::Rng;
 
 pub struct Instruction {
     opcode: u16,
@@ -38,11 +39,11 @@ impl Instruction {
                 0x800E => self.put_value_of_bitwise_shift_left_operation_between_v_registers_on_first_passed_register(cpu),
                 _ => (),
             },
-            // 0x9000 => self.skipNextIfFirstRegisterValueDiffSecondRegisterValue(cpu),
-            // 0xA000 => self.loadValueOnIRegister(cpu),
-            // 0xB000 => self.jumpToPassedAddressPlusV0(cpu),
-            // 0xC000 => self.storeValueOfBitwiseAndBetweenPassedArgumentAndRandomByte(cpu),
-            // 0xD000 => self.drawByteSpriteStartingAtLocationIOnRegisterStoredLocation(cpu, memory, display),
+            0x9000 => self.skip_next_instruction_if_v_registers_values_are_different(cpu),
+            0xA000 => self.put_argument_value_on_i_register(cpu),
+            0xB000 => self.jump_to_argument_value_plus_v0(cpu),
+            0xC000 => self.put_value_of_bitwise_or_operation_between_argument_and_random_byte_on_passed_v_register(cpu),
+            0xD000 => self.draw_byte_sprite_starting_at_location_i_on_register_stored_location(cpu),
             _ => (),
         }
     }
@@ -267,6 +268,54 @@ impl Instruction {
 
         cpu.set_v_register(0xF, (value >> 7) & 0x1);
         cpu.set_v_register(first_register_number, value);
+    }
+
+    fn skip_next_instruction_if_v_registers_values_are_different(&self, cpu: &mut Cpu) {
+        println!("skip_next_instruction_if_v_registers_values_are_different");
+
+        let first_register_number: usize = (self.opcode as usize) >> 8 & 0x000F;
+        let second_register_number: usize = (self.opcode as usize) >> 4 & 0x000F;
+
+        let first_register_value: u8 = cpu.get_v_register(first_register_number);
+        let second_register_value: u8 = cpu.get_v_register(second_register_number);
+
+        if first_register_value != second_register_value {
+            cpu.increase_program_counter(0x2);
+        }
+    }
+
+    fn put_argument_value_on_i_register(&self, cpu: &mut Cpu) {
+        println!("put_argument_value_on_i_register");
+
+        let argument: u16 = self.opcode & 0xFFF;
+
+        cpu.set_i_register(argument);
+    }
+
+    fn jump_to_argument_value_plus_v0(&self, cpu: &mut Cpu) {
+        println!("put_argument_value_on_i_register");
+
+        let argument: u16 = self.opcode & 0xFFF;
+        let address: usize = (argument as usize) + (cpu.get_v_register(0x0) as usize);
+
+        cpu.set_program_counter(address);
+    }
+
+    fn put_value_of_bitwise_or_operation_between_argument_and_random_byte_on_passed_v_register(&self, cpu: &mut Cpu) {
+        println!("put_value_of_bitwise_or_operation_between_argument_and_random_byte_on_passed_v_register");
+
+        let register_number: usize = ((self.opcode as usize) >> 8) & 0xF;
+        let argument: u8 = (self.opcode as u8) & 0xFF;
+        let random_byte: u8 = rand::thread_rng().gen_range(0x0..0xFF);
+
+        cpu.set_v_register(
+            register_number,
+            argument | random_byte
+        );
+    }
+
+    fn draw_byte_sprite_starting_at_location_i_on_register_stored_location(&self, cpu: &mut Cpu) {
+        println!("draw_byte_sprite_starting_at_location_i_on_register_stored_location");
     }
 }
 
@@ -550,5 +599,62 @@ mod tests {
 
         assert_eq!(result, cpu.get_v_register(0xA));
         assert_eq!(flag, cpu.get_v_register(0xF));
+    }
+
+    #[test_case(0xFF, 0xAA, 0x202 ; "with different values")]
+    #[test_case(0xFF, 0xFF, 0x200 ; "with the same value")]
+    fn it_should_skip_next_instruction_if_v_registers_values_are_different(
+        first_register_value: u8, 
+        second_register_value: u8, 
+        program_counter: usize
+    ) {
+        let mut instruction: Instruction = Instruction::initialize(0x9A, 0xB0);
+        let mut cpu: Cpu = Cpu::initialize();
+
+        cpu.set_v_register(0xA, first_register_value);
+        cpu.set_v_register(0xB, second_register_value);
+
+        instruction.interpret(&mut cpu);
+
+        assert_eq!(program_counter, cpu.get_program_counter());
+    }
+
+    #[test]
+    fn it_should_put_argument_value_on_i_register() {
+        let mut instruction: Instruction = Instruction::initialize(0xAC, 0xAF);
+        let mut cpu: Cpu = Cpu::initialize();
+
+        instruction.interpret(&mut cpu);
+
+        assert_eq!(0x0CAF, cpu.get_i_register());
+    }
+
+    #[test]
+    fn it_should_jump_to_argument_value_plus_v0() {
+        let mut instruction: Instruction = Instruction::initialize(0xB0, 0x03);
+        let mut cpu: Cpu = Cpu::initialize();
+
+        cpu.set_v_register(0x0, 0x4);
+
+        instruction.interpret(&mut cpu);
+
+        assert_eq!(0x7, cpu.get_program_counter());
+    }
+
+    #[test]
+    fn it_should_put_value_of_bitwise_or_operation_between_argument_and_random_byte_on_passed_v_register() {
+        let mut instruction: Instruction = Instruction::initialize(0xCA, 0xFF);
+        let mut cpu: Cpu = Cpu::initialize();
+
+        assert_eq!(0x0, cpu.get_v_register(0xA));
+
+        instruction.interpret(&mut cpu);
+
+        assert_eq!(0xFF, cpu.get_v_register(0xA));
+    }
+
+    #[test]
+    fn it_should_draw_byte_sprite_starting_at_location_i_on_register_stored_location() {
+        let instruction: Instruction = Instruction::initialize(0xDA, 0xCF);
     }
 }
